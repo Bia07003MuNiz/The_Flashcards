a1501fd3f90fb6a3278b3de90e283dfdc4533ee4
TalissaOliveira <talissaoliveira2607@gmail.com>
2023-02-25 14:35:23 -0300

    Ajustes

--- resources/views/components/layout-base.blade.php
+++ resources/views/components/layout-base.blade.php
@@ -1,24 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title ?? 'SisCongelado' }}</title>
-
+    <title>{{ $title ?? 'Congelados' }}</title>
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-
     @stack('scss')
-
 </head>
 <body>
-
-    <div class="container">
-        @include('navbar')
-        {{ $slot }}
-    </div>
-
-
-    
+    @include('header')
+    {{ $slot }}
+    @include('footer')
 </body>
 </html>
