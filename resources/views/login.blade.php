63dcbcc0aae8850e2a0b4aae9fc30c9a547f4f01
Bianca Muniz <bianca.muniz@fatec.sp.gov.br>
2023-03-25 00:07:06 -0300

    seu comentario

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,44 +7,50 @@
         <script src="https://cdn.tailwindcss.com"></script>
     @endpush
 
-
-    <div class="container">
-        <div class="h-screen p-6">
-            <form action="{{route('logar')}}" method="POST" class="m-auto rounded-lg p-6 bg-white  border border-gray-400">
-                @csrf
-                <div class="text-left text-xl font-bold" style= "padding-left:2%; color:#4C1D95">LOGIN</div>
-                <div class="text-center text-xl font-bold" style= "padding-left:2%; color:black; font-size: 90%;">Entre com seus dados</div>
-               
-                <div>
-                    <p class="align-middle" style=" display: inline-block; vertical-align: middle; ">Entrar como:</p>
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="opcao1">
-                    <label class="form-check-label" for="inlineRadio1">Cliente</label>
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="opcao2">
-                    <label class="form-check-label" for="inlineRadio2">Vendedor</label>
+    <div class="bg-white w-screen h-screen flex items-center justify-center">
+        <div class="bg-white rounded-lg shadow-lg p-8 w-96">
+            <h1 class="text-2xl font-bold text-purple-900 text-center mb-8">LOGIN</h1>
+            <div class="mb-6">
+                    <div class="text-gray-700 font-bold mb-2">
+                        Entrar como:
+                    </div>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio" name="inlineRadioOptions" id="inlineRadio1" value="opcao1">
+                        <span class="ml-2 text-gray-700">Cliente</span>
+                    </label>
+                    <label class="inline-flex items-center ml-6">
+                        <input type="radio" class="form-radio" name="inlineRadioOptions" id="inlineRadio2" value="opcao2">
+                        <span class="ml-2 text-gray-700">Vendedor</span>
+                    </label>
                 </div>
-
-     
-                @error('email')
-                    <div class="alert alert-danger">{{$message}}</div>
-                @enderror
-                <div class="col-6">
-                <input class="ml-2 w-full rounded-lg border px-4 placeholder-blue-500" type="text" name="email" id="email" placeholder="E-mail" />
-
+            <form action="{{route('logar')}}" method="POST">
+                @csrf
+                <div class="mb-4">
+                    <label class="block text-gray-700 font-bold mb-2" for="email">
+                                           </label>
+                    <input class="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-400" id="email" name="email" type="email" placeholder="E-mail" />
+                    @error('email')
+                        <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 font-bold mb-2" for="password">
+                    </label>
+                    <input class="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-400" id="password" name="password" type="password" placeholder="Senha" />
+                    @error('password')
+                        <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
                 
-                @error('password')
-                    <div class="alert alert-danger">Senha é obrigatório!</div>
-                @enderror
-               <div class="col-6">
-                     <input class="ml-2 w-full rounded-lg border px-4 placeholder-blue-500" type="password" name="password" id="password" placeholder="Senha" />
-               </div>
-            
-                <div class="mt-11 flex justify-center">
-  <button type="submit" class="rounded-md bg-purple-900 p-1 font-bold text-white mr-2 h-8">CRIAR CONTA</button>
-  <button type="submit" class="rounded-md bg-purple-900 p-1 font-bold text-white ml-2 h-8">ENTRAR</button>
-</div>
-                </form>
-          </div>
-          
+                <div class="flex justify-center">
+                    <button type="submit" class="bg-purple-800  p-1 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg mr-2">
+                        CRIAR CONTA
+                    </button>
+                    <button type="button" class="bg-purple-800  p-1 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg ml-2">
+                        ENTRAR
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </x-layout-base>
