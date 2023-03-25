feb5ed17f33c9a33900e935551219fae31f18e2c
Bianca Muniz <bianca.muniz@fatec.sp.gov.br>
2023-03-25 17:46:52 -0300

    atualizado tela de login e duvidas

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,51 +7,59 @@
         <script src="https://cdn.tailwindcss.com"></script>
     @endpush
 
-    <div class="bg-white w-screen h-screen flex items-center justify-center">
-        <div class="bg-white rounded-lg shadow-lg p-8 w-96">
-            <h1 class="text-2xl font-bold text-purple-900 text-left mb-8">LOGIN</h1>
-            <div class="text-center text-xl font-bold" style= "padding-left:2%; color:black; font-size: 90%;">Entre com seus dados</div>
-            <div class="mb-6">
-                    <div class="text-blue-600 font-bold mb-2">
-                        Entrar como:
-                    </div>
-                    <label class="inline-flex items-center">
-                        <input type="radio" class="form-radio" name="inlineRadioOptions" id="inlineRadio1" value="opcao1">
-                        <span class="ml-2 text-blue-600">Cliente</span>
-                    </label>
-                    <label class="inline-flex items-center ml-6">
-                        <input type="radio" class="form-radio " name="inlineRadioOptions" id="inlineRadio2" value="opcao2">
-                        <span class="ml-2 text-blue-600">Vendedor</span>
-                    </label>
-                </div>
-            <form action="{{route('logar')}}" method="POST">
-                @csrf
-                <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="email">
-                                           </label>
-                    <input class="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  placeholder-blue-500" id="email" name="email" type="email" placeholder="E-mail" />
-                    @error('email')
-                        <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="password">
-                    </label>
-                    <input class="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  placeholder-blue-500" id="password" name="password" type="password" placeholder="Senha" />
-                    @error('password')
-                        <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                
-                <div class="flex justify-center">
-                    <button type="submit" class="bg-purple-900  p-1 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg mr-2">
-                        CRIAR CONTA
-                    </button>
-                    <button type="button" class=" bg-purple-900  p-1 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg ml-2">
-                        ENTRAR
-                    </button>
-                </div>
-            </form>
+    <div class="container">
+    <div class="bg-white w-1200 h-800 flex items-center justify-center">
+  <div class="bg-white shadow-lg p-10 w-1006">
+    <h1 class="text-2xl font-bold text-purple-900 text-left mb-8" style="font-size:40px;">LOGIN</h1>
+    <div class="text-center text-xl font-bold" style="color:black; font-size: 20px;">Entre com seus dados</div>
+    <br><br>
+    <div class="mb-6">
+      <div class="text-blue-700 font-bold mb-2">
+        <div class="flex justify-center">
+          Entrar como:
+          <label class="inline-flex items-center">
+            <input type="radio" class="form-radio" name="inlineRadioOptions" id="inlineRadio1" value="opcao1">
+            <span class="ml-2 text-blue-600">Cliente</span>
+          </label>
+          <label class="inline-flex items-center ml-6">
+            <input type="radio" class="form-radio" name="inlineRadioOptions" id="inlineRadio2" value="opcao2">
+            <span class="ml-2 text-blue-600">Vendedor</span>
+          </label>
         </div>
+        <form action="{{route('logar')}}" method="POST">
+    @csrf
+    <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="email"></label>
+        <input class="appearance-none border rounded-md border-blue-500 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-blue-500" id="email" name="email" type="email" placeholder="E-mail" />
+        @error('email')
+        <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
+        @enderror
     </div>
+    <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="password"></label>
+        <input class="appearance-none border rounded-md border-blue-500 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-blue-500" id="password" name="password" type="password" placeholder="Senha" />
+        @error('password')
+        <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
+        @enderror
+    </div>
+</form> 
+
+          <div class="flex justify-end mb-2">
+            <a href="#" class="btn btn-link text-black text-sm">Esqueceu sua senha?</a>
+          </div>
+          <div class="flex justify-center">
+            <button type="submit" class="bg-purple-900 p-1 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg mr-2">
+              CRIAR CONTA
+            </button>
+            <button type="button" class="bg-purple-900 p-1 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg ml-2">
+              ENTRAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 </x-layout-base>
--- resources/views/quem-somos.blade.php
+++ resources/views/quem-somos.blade.php
@@ -6,6 +6,46 @@
         <div class="container">
             <h1>Quem somos</h1>
             <p></p>
+<body>
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="pills-missao-tab" data-toggle="pill" href="#pills-missao" role="tab" aria-controls="pills-missao" aria-selected="true">Missão</a>
+    
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-visao-tab" data-toggle="pill" href="#pills-visao" role="tab" aria-controls="pills-visao" aria-selected="false">Visão</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-valores-tab" data-toggle="pill" href="#pills-valores" role="tab" aria-controls="pills-valores" aria-selected="false">Valores</a>
+  </li>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="pills-missao" role="tabpanel" aria-labelledby="pills-missao-tab">A missão da nossa empresa de distribuição de congelados é fornecer aos nossos clientes 
+                produtos de qualidade superior, com um serviço excepcional e entregas pontuais e confiáveis.
+                Acreditamos que, ao fazer isso, podemos ajudar nossos clientes a atender às demandas de
+                seus próprios negócios, proporcionando-lhes uma ampla seleção de produtos congelados de
+                alta qualidade e uma logística eficiente. Estamos comprometidos em manter altos padrões
+                de qualidade e segurança alimentar, bem como em trabalhar com nossos fornecedores e
+                parceiros de forma ética e responsável.</div>
+
+  <div class="tab-pane fade" id="pills-visao" role="tabpanel" aria-labelledby="pills-visao-tab">Ser reconhecida como uma das principais referências no mercado, oferecendo um atendimento diferenciado e produtos de 
+                  qualidade, mantendo um crescimento sustentável e diversificado, ampliando a atuação em diferentes regiões e segmentos
+                  do mercado e contribuindo para o desenvolvimento econômico e social do país.</div>
+
+  <div class="tab-pane fade" id="pills-valores" role="tabpanel" aria-labelledby="pills-valores-tab"><b>Qualidade:</b> nos esforçamos para fornecer alimentos congelados da mais alta qualidade, utilizando apenas ingredientes frescos e seguros em nossos produtos.<br>
+                  <b>Segurança alimentar:</b> mantemos altos padrões de segurança alimentar em todas as etapas da produção, desde a seleção de ingredientes até o armazenamento e distribuição dos nossos produtos.<br>
+  </div>
+</div>
+
+
+
+
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
+</body>
+        
     </main>
 </x-layout-base>
