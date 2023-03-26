c6a5d731cdfec5cb5b09ae14d0211fe099cbabca
unknown <bianca.muniz@fatec.sp.gov.br>
2023-03-26 16:10:48 -0300

    lista de produto e sobre nos

--- resources/views/quem-somos.blade.php
+++ resources/views/quem-somos.blade.php
@@ -1,22 +1,46 @@
+
 <x-layout-base>
-    <x-slot:title>
-        Quem somos
-    </x-slot>
-    <main id="quem-somos">
-        <div class="container">
-            <h1>Quem somos</h1>
-            <p></p>
-<body>
-            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="pills-missao-tab" data-toggle="pill" href="#pills-missao" role="tab" aria-controls="pills-missao" aria-selected="true">Missão</a>
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./resources/sass/quem-somos.scss">
+  </head>
+  <body>
+
+  <div class="container">
+    
+  <div class="row">
+    <div class="col-texto">
+      <br><p>Somos uma empresa que se dedica a vender alimentos que foram congelados para sua conservação.
+                  Estes alimentos podem incluir carnes, pescados, frutas, verduras, pratos preparados, entre outros produtos.Nossa instalações 
+                  são adequadas para o armazenamento e processamentodos alimentos, bem como com um equipamento de 
+                  profissionais especializados na produção de alimentos congelados. </p>
+                <p>Entre as vantagens dos alimentos congelados está sua longa vida útil, que permite maior
+                  flexibilidade na gestão da cadeia de suprimentos e no planejamento da produção. Além disso
+                  Esses alimentos são mais convenientes para o consumidor, pois podem ser armazenados no freezer e consumidos a 
+                  qualquer hora sem preocupação com dados prudenciais.</p>  
+    </div>
+    <div class="col">
+   
+              <img class="img-fluid" src="assets/images/produtos/sobrenos.jpg"  alt="imagem 1">
+    </div>
+   
+  </div>
+
+  <br><br> <br>
+  <ul class="nav  mb-6" id="pills-tab" role="tablist">
+  <li class="nav-item-bianca">
+    <a class="nav-link active" id="pills-missao-tab" data-toggle="pill" href="#pills-missao" role="tab" aria-controls="pills-missao" aria-selected="true"><font color="white"> MISSÃO</font></a>
     
   </li>
-  <li class="nav-item">
-    <a class="nav-link" id="pills-visao-tab" data-toggle="pill" href="#pills-visao" role="tab" aria-controls="pills-visao" aria-selected="false">Visão</a>
+  <li class="nav-item-bianca">
+    <a class="nav-link" id="pills-visao-tab" data-toggle="pill" href="#pills-visao" role="tab" aria-controls="pills-visao" aria-selected="false"><font color="white"> VISÃO</font></a>
   </li>
-  <li class="nav-item">
-    <a class="nav-link" id="pills-valores-tab" data-toggle="pill" href="#pills-valores" role="tab" aria-controls="pills-valores" aria-selected="false">Valores</a>
+  <li class="nav-item-bianca">
+    <a class="nav-link" id="pills-valores-tab" data-toggle="pill" href="#pills-valores" role="tab" aria-controls="pills-valores" aria-selected="false"><font color="white"> VALORES</font></a>
   </li>
 </ul>
 <div class="tab-content" id="pills-tabContent">
@@ -44,8 +68,15 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+ 
+<BR><BR><BR>
+</div> 
+<button class="btn-b btn-primary" type="submit"><a href="http://127.0.0.1:8000"><font color="white">VOLTAR</font></button>   
 
-</body>
+  </body>
+</html>
+       
+
         
     </main>
 </x-layout-base>
