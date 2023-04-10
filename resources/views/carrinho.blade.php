8a4dd947d8c8f21f25dcf4a28b2d49351bb13836
Arthur M. W. Rebelo <arthur-rebelo@hotmail.com.br>
2023-04-10 19:41:53 -0300

    arthur 10.04.23

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -2,7 +2,19 @@
     <x-slot:title>
         Carrinho
     </x-slot>
+
+
     <div class="container">
+        <div class="titulo">
+            <span>Orçamento</span>
+        </div>
+    
+        <div id="retangulo">
+
+        </div>
+
+        <button class="btn btn-primary btn__style" type="submit">ADICIONAR MAIS PRODUTOS</button>
+        <button class="btn btn-primary btn__style" type="submit">SOLICITAR ORÇAMENTO</button>
         
     </div>
 </x-layout-base>
