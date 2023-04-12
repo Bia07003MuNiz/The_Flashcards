b22beb3e76a19f9ad7efa562cf66b2657865e205
Talissa Oliveira <programador4gv8@gmail.com>
2023-04-12 16:09:51 -0300

    Ajustes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,37 +1,38 @@
 <header id="topo">
   <div class="box__frase">
     <div class="container">
-      Seja bem vindo(a)@auth, {{Auth::user()->nome}}@endauth
+      Seja bem vindo(a)@auth vendedor(a) {{Auth::user()->nome}}@endauth
     </div>
   </div>
   <div class="box__menu">
     <div class="container container__style">
       <div class="dropdown">
         <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img src="assets/svg/menu.svg" width="45" height="40" alt="Ícone menu">
+          <img src="http://127.0.0.1:8000/assets/svg/menu.svg" width="45" height="40" alt="Ícone menu">
         </button>
         <div class="dropdown-menu dropdown__style" aria-labelledby="dropdownMenuButton">
           <a class="dropdown-item item__style {{ Request::is('/') ? 'active' : '' }}" href="{{route('home')}}">Home</a>
           <a class="dropdown-item item__style {{ Request::is('quem-somos') ? 'active' : '' }}" href="{{route('quem-somos')}}">Quem somos</a>
-          <a class="dropdown-item item__style {{ Request::is('categorias*') ? 'active' : '' }}" href="{{route('categorias.index')}}">Categorias</a>
           <a class="dropdown-item item__style {{ Request::is('produtos*') ? 'active' : '' }}" href="{{route('produtos.index')}}">Produtos</a>
           <a class="dropdown-item item__style {{ Request::is('onde-estamos') ? 'active' : '' }}" href="{{route('onde-estamos')}}">Localização</a>
           <a class="dropdown-item item__style {{ Request::is('contato') ? 'active' : '' }}" href="{{route('contato')}}">Contato</a>
         </div>
       </div>
       <a href="{{route('home')}}">
-        <img src="assets/images/logo.png" class="img-fluid" alt="Logo" width="250" height="38">
+        <img src="http://127.0.0.1:8000/assets/images/logo.png" class="img-fluid" alt="Logo" width="250" height="38">
       </a>
       <div class="main__lateral">
         @auth
           <div class="dropdown">
             <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Área restrita
+              <i class="fa-solid fa-user"></i>
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item {{ Request::is('meus-dados') ? 'active' : '' }}" href="{{route('meus-dados')}}">Meus dados</a>
+              <a class="dropdown-item {{ Request::is('alterar-senha') ? 'active' : '' }}" href="{{route('alterar-senha')}}">Alterar senha</a>
+              <a class="dropdown-item {{ Request::is('categorias*') ? 'active' : '' }}" href="{{route('categorias.index')}}">Categorias</a>
+              <a class="dropdown-item {{ Request::is('produtos*') ? 'active' : '' }}" href="{{route('produtos.index')}}">Produtos</a>
+              <a class="dropdown-item" href="{{route('logout')}}">Sair</a>
             </div>
           </div>
         @else
