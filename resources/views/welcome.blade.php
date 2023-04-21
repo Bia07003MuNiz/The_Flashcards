fe960e598ee8e8fc5ed588af92f707b839c7e655
Talissa Oliveira <programador4gv8@gmail.com>
2023-04-20 23:21:30 -0300

    Ajustes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,17 +12,17 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="10000">
-                        <a href="google.com" target="_blank" rel="noopener" title="Banner">
+                        <a href="https://google.com" target="_blank" rel="noopener" title="Banner">
                             <img src="assets/images/Banner.jpg" class="d-block w-100 img-fluid" alt="Banner" width="1920" height="600">
                         </a>
                     </div>
                     <div class="carousel-item" data-bs-interval="20000">
-                        <a href="google.com" target="_blank" rel="noopener" title="Banner">
+                        <a href="https://google.com" target="_blank" rel="noopener" title="Banner">
                             <img src="assets/images/Banner.jpg" class="d-block w-100 img-fluid" alt="Banner" width="1920" height="600">
                         </a>
                     </div>
                     <div class="carousel-item" data-bs-interval="30000">
-                        <a href="google.com" target="_blank" rel="noopener" title="Banner">
+                        <a href="https://google.com" target="_blank" rel="noopener" title="Banner">
                             <img src="assets/images/Banner.jpg" class="d-block w-100 img-fluid" alt="Banner" width="1920" height="600">
                         </a>
                     </div>
